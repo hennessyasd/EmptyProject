2a5bb59b7723ea7a27b3913a68a065f2ee6d8bac
hennessyasd <igor10212005@gmail.com>
2022-03-29 18:23:56 +0300

    Update Main.cpp

--- EmptyProject/Main.cpp
+++ EmptyProject/Main.cpp
@@ -2,18 +2,34 @@
 
 using namespace std;
 
+//#define SIZE 5;
+
 int main() {
 
-	int a, b;
+	int* array = new int[5];
 
-	cin >> a >> b;
-
-	if (a > b) {
-		cout << a;
-	}
-	else {
-		cout << b;
+	for (int i = 0; i < 5; i++) { // prog dont wanna use define "SIZE" :(
+		cin >> array[i] ;
 	}
 
+	for (int i = 0; i < 5 - 1; i++){
+		for (int k = 0; k < 5 - i - 1; k++) {
+			if (array[k] > array[k + 1]) {
+				int temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		cout << array[i] << " ";
+	}
+
+	cout << endl;
+
+	delete[] array;
+
+	system("pause");
 	return 0; // all prog
 }
