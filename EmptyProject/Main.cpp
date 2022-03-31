52e7547f45dfed07c6ce8284294278ad203ed2c9
hennessyasd <igor10212005@gmail.com>
2022-03-31 17:28:47 +0300

    Update my project

--- EmptyProject/Main.cpp
+++ EmptyProject/Main.cpp
@@ -2,33 +2,9 @@
 
 using namespace std;
 
-//#define SIZE 5;
-
 int main() {
 
-	int* array = new int[5];
 
-	for (int i = 0; i < 5; i++) { // prog dont wanna use define "SIZE" :(
-		cin >> array[i] ;
-	}
-
-	for (int i = 0; i < 5 - 1; i++){
-		for (int k = 0; k < 5 - i - 1; k++) {
-			if (array[k] > array[k + 1]) {
-				int temp = array[k];
-				array[k] = array[k + 1];
-				array[k + 1] = temp;
-			}
-		}
-	}
-
-	for (int i = 0; i < 5; i++) {
-		cout << array[i] << " ";
-	}
-
-	cout << endl;
-
-	delete[] array;
 
 	system("pause");
 	return 0;
---
+++ EmptyProject/Plane.h
@@ -0,0 +1,8 @@
+#include "Header.h"
+
+class Plane {
+	public:
+		double pice;
+		string name;
+		string model;
+};
